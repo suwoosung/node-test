ee12d693b2735fe78084592d160caf5b4c715488
USER <USER@DESKTOP-OQRSULT>
2023-12-03 01:26:35 +0900

    this is commit message

---
+++ User.java
@@ -0,0 +1,11 @@
+package com.market.member;
+
+public class User extends Person {
+	
+	public User(string name, int phone) {
+		super(name, phone);
+	}
+	public User(string username, int phone, String address) {
+		super(username, phone, address);
+	}
+}
